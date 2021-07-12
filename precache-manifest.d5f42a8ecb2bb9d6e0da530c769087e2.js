9c7a7c8c14627277f4b919bb3b5618c0a3346165
ototom <to.krawczyk@gmail.com>
2021-07-12 21:04:46 +0200

    Updates

---
+++ precache-manifest.d5f42a8ecb2bb9d6e0da530c769087e2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15bbf3eba5a836a7b3ba609e5d2db988",
+    "url": "/products/index.html"
+  },
+  {
+    "revision": "1221b93d265660c628bf",
+    "url": "/products/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "38c572f8e3a6914961e8",
+    "url": "/products/static/css/main.75ee5472.chunk.css"
+  },
+  {
+    "revision": "1221b93d265660c628bf",
+    "url": "/products/static/js/2.2be6f67e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/products/static/js/2.2be6f67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c572f8e3a6914961e8",
+    "url": "/products/static/js/main.3c192439.chunk.js"
+  },
+  {
+    "revision": "ca711005868cab90f72e",
+    "url": "/products/static/js/runtime-main.cadc2fe9.js"
+  },
+  {
+    "revision": "a65a8a8cd20ecfc7f63ac7540a6df40e",
+    "url": "/products/static/media/bag-icon.a65a8a8c.svg"
+  },
+  {
+    "revision": "f655ec08fb515845062fe15f2b9b20cc",
+    "url": "/products/static/media/check.f655ec08.svg"
+  },
+  {
+    "revision": "a594ab6047fb39e46b538aa2f6e8f160",
+    "url": "/products/static/media/close.a594ab60.svg"
+  },
+  {
+    "revision": "2f01c39029a0b6a52b54b1e29c9ea9ef",
+    "url": "/products/static/media/empty-star.2f01c390.svg"
+  },
+  {
+    "revision": "6bf634aa06cf3e3a51a687ba955feb2a",
+    "url": "/products/static/media/filled-star.6bf634aa.svg"
+  },
+  {
+    "revision": "a20bd547c408b4b418aaf8247fbf4b16",
+    "url": "/products/static/media/login-bg.a20bd547.jpg"
+  },
+  {
+    "revision": "1f9315f458a07c149cf551488e058beb",
+    "url": "/products/static/media/search.1f9315f4.svg"
+  }
+]);
